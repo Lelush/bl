7d7babc8676b07dfc66e7b44fa266b0b77493710
YuryT <htmlcoder001@gmail.com>
2017-11-16 16:14:17 +0300

    Five slide fix

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -85,7 +85,7 @@ $this->title = 'BL';
                   Зарабатывайте, получайте скидки и становитесь популярнее
                 </span>
             </div>
-            <img class="hidden-xs hidden-sm" src="http://static.blrevolution.com/img/desktop/desktop-5-new.jpg"/>
+            <img class="hidden-xs hidden-sm" src="http://static.blrevolution.com/img/desktop/desktop-5-new.png"/>
             <div class="slide-content">
                 <a class="bl_button" href="#">Начать</a>
             </div>
